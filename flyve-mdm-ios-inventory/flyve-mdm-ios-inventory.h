c3efbd38c49797386f61df1d7d04685a58ef8e69
Hector Rondon <hectorerb@gmail.com>
2017-06-06 11:28:17 -0400

    feat(project): init xcode project

---
+++ flyve-mdm-ios-inventory/flyve-mdm-ios-inventory.h
@@ -0,0 +1,19 @@
+//
+//  flyve-mdm-ios-inventory.h
+//  flyve-mdm-ios-inventory
+//
+//  Created by Hector Rondon on 06/06/17.
+//  Copyright © 2017 s42. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for flyve-mdm-ios-inventory.
+FOUNDATION_EXPORT double flyve_mdm_ios_inventoryVersionNumber;
+
+//! Project version string for flyve-mdm-ios-inventory.
+FOUNDATION_EXPORT const unsigned char flyve_mdm_ios_inventoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <flyve_mdm_ios_inventory/PublicHeader.h>
+
+
