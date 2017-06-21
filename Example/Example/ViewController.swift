29ba59aaf69b186e15fe427139011c690c718497
Hector Rondon <hrondon@teclib.com>
2017-06-21 12:37:39 -0400

    add createXmlFile to example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -29,16 +29,115 @@ import UIKit
 import FlyveMDMInventory
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    lazy var xmlButton: UIButton = {
+        
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.backgroundColor = UIColor.init(red: 6.0/255.0, green: 135.0/255.0, blue: 133.0/255.0, alpha: 1.0).cgColor
+        button.setTitle("XML", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.addTarget(self, action: #selector(self.generateXML), for: .touchUpInside)
+        return button
+    }()
+    
+    let messageLabel: UILabel = {
+        
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = ""
+        label.sizeToFit()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.backgroundColor = .clear
+        label.textColor = .gray
+        
+        return label
+    }()
+    
+    let loadingIndicatorView: UIActivityIndicatorView = {
+        
+        let loading = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        
+        loading.translatesAutoresizingMaskIntoConstraints = false
+        loading.hidesWhenStopped = true
+        
+        return loading
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        
+        setupViews()
+        addConstraints()
+    }
+    
+    func setupViews() {
+        
+        view.backgroundColor = .white
+        view.addSubview(xmlButton)
+        view.addSubview(messageLabel)
+        view.addSubview(loadingIndicatorView)
+    }
+    
+    func addConstraints() {
+        
+        xmlButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        xmlButton.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        xmlButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        xmlButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        loadingIndicatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loadingIndicatorView.topAnchor.constraint(equalTo: xmlButton.bottomAnchor, constant: 16.0).isActive = true
+        
+        messageLabel.bottomAnchor.constraint(equalTo: xmlButton.topAnchor, constant: -16.0).isActive = true
+        messageLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24).isActive = true
+        messageLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24).isActive = true
+    }
+    
+    func generateXML() {
+        
+        messageLabel.text = ""
+        loadingIndicatorView.startAnimating()
         
         let inventoryTask = InventoryTask()
         
         inventoryTask.execute("Agent_v1.0") { result in
             
-            print(result)
+            createXmlFile(result)
         }
+    }
+    
+    func createXmlFile(_ xml: String) {
+        
+        let fileName = "inventory.xml"
+        
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            
+            let path = dir.appendingPathComponent(fileName)
+            
+            print(path)
+
+            do {
+                try xml.write(to: path, atomically: false, encoding: String.Encoding.utf8)
+                
+                messageLabel.text = "inventory.xml file created successfully\n\(path)"
+            }
+            catch {
+                debugPrint("Error output xml")
+            }
+            
+            //reading
+            do {
+                let text2 = try String(contentsOf: path, encoding: String.Encoding.utf8)
+                
+                print(text2)
+            }
+            catch {/* error handling here */}
+        }
+        
+        loadingIndicatorView.stopAnimating()
     }
 
     override func didReceiveMemoryWarning() {
