c664014e834263cd0dc53af63fe6b6ad4c4266e1
Hector Rondon <hrondon@teclib.com>
2017-07-06 18:27:21 -0400

    fix more warnings from swiftlint

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -32,14 +32,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        
         window?.rootViewController = ViewController()
-        
+
         return true
     }
 
@@ -64,7 +62,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
