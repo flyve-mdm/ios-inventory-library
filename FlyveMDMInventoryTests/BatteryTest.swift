73cce7319a275912b8f138826364f5d97c1090ba
Hector Rondon <hrondon@teclib.com>
2017-07-06 14:25:33 -0400

    test(unit): create unit tests (#41)
    * Cast type to correct format
    * Change devicesWithMediaType for AVCaptureDeviceDiscoverySession instead
    * Create memory unit test
    * Create storage unit test
    * Create hardware unit test
    * Create network unit test
    * Create battery unit test
    * Create carrier unit test
    * Create operating system unit test
    * Create CPU unit test
    * Update cocoapods example
    Close: https://github.com/flyve-mdm/flyve-mdm-ios-inventory/issues/9

---
+++ FlyveMDMInventoryTests/BatteryTest.swift
@@ -0,0 +1,33 @@
+//
+//  BatteryTest.swift
+//  FlyveMDMInventory
+//
+//  Created by Hector Rondon on 06/07/17.
+//  Copyright © 2017 Teclib. All rights reserved.
+//
+
+import XCTest
+import FlyveMDMInventory
+
+class BatteryTest: XCTestCase {
+    
+    let battery = Battery()
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testGetState() {
+        XCTAssertNotNil(battery.state(), "Battery state not valid")
+    }
+    
+    func testGetLevel() {
+        XCTAssertNotNil(battery.level(), "Battery level not valid")
+    }
+}
---
+++ FlyveMDMInventoryTests/NetworkTest.swift
@@ -0,0 +1,55 @@
+//
+//  NetworkTest.swift
+//  FlyveMDMInventory
+//
+//  Created by Hector Rondon on 06/07/17.
+//  Copyright © 2017 Teclib. All rights reserved.
+//
+
+import XCTest
+import FlyveMDMInventory
+
+class NetworkTest: XCTestCase {
+    
+    let network = Network()
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testGetType() {
+        
+        XCTAssertNotNil(network.type(), "Network type not valid")
+    }
+    
+    func testGetSsid() {
+        
+        XCTAssertNotNil(network.ssid(), "Network ssid not valid")
+    }
+    
+    func testGetBssid() {
+        
+        XCTAssertNotNil(network.bssid(), "Network basic service set identifier not valid")
+    }
+    
+    func testGetLocalIPAddress() {
+        
+        XCTAssertNotNil(network.localIPAddress(), "Network local IP address not valid")
+    }
+    
+    func testGetBroadcastAddress() {
+        
+        XCTAssertNotNil(network.broadcastAddress(), "Network broadcast address not valid")
+    }
+    
+    func testGetMacAddress() {
+        
+        XCTAssertNotNil(network.type(), "Network MAC address not valid")
+    }
+}
