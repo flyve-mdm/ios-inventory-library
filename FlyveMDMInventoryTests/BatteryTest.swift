f4bf674f7b23f1a874735caa2a9e9cabe8e544a0
Naylin <nmedina@teclib.com>
2017-09-13 18:00:20 -0400

    docs(setUp): change the description

--- FlyveMDMInventoryTests/BatteryTest.swift
+++ FlyveMDMInventoryTests/BatteryTest.swift
@@ -13,9 +13,9 @@ class BatteryTest: XCTestCase {
 
     let battery = Battery()
 
+    /// This method is called before the invocation of each test method in the class.
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
