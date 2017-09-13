567059bb8e8e899ada1b7fe2eb108c394ab7230a
Naylin <nmedina@teclib.com>
2017-09-13 18:04:18 -0400

    docs(tearDown): change the description

--- FlyveMDMInventoryTests/CarrierTest.swift
+++ FlyveMDMInventoryTests/CarrierTest.swift
@@ -18,8 +18,8 @@ class CarrierTest: XCTestCase {
         super.setUp()
     }
 
+    /// This method is called after the invocation of each test method in the class.
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
