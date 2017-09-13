2e7a09eb3b3a5544a07933ed60fff7a520fc9b8b
Naylin <nmedina@teclib.com>
2017-09-13 17:55:38 -0400

    docs(tearDown): change description

--- FlyveMDMInventoryTests/MemoryTest.swift
+++ FlyveMDMInventoryTests/MemoryTest.swift
@@ -18,8 +18,8 @@ class MemoryTest: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    /// This method is called after the invocation of each test method in the class.
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
