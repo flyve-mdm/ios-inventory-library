2a4869c7fc0d587c2113fe8b558909f5a69f3278
Naylin <nmedina@teclib.com>
2017-09-14 15:27:44 -0400

    docs(setUp): change description

--- FlyveMDMInventoryTests/FlyveMDMInventoryTests.swift
+++ FlyveMDMInventoryTests/FlyveMDMInventoryTests.swift
@@ -30,9 +30,9 @@ import XCTest
 
 class FlyveMDMInventoryTests: XCTestCase {
 
+    /// This method is called before the invocation of each test method in the class.
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
