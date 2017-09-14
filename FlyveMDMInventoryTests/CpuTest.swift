1f671170af9dbcbcdc6dd33bd3d43df5d5ca4b29
Naylin <nmedina@teclib.com>
2017-09-14 14:28:47 -0400

    docs(setUp): change the description

--- FlyveMDMInventoryTests/CpuTest.swift
+++ FlyveMDMInventoryTests/CpuTest.swift
@@ -13,9 +13,9 @@ class CpuTest: XCTestCase {
 
     let cpu = Cpu()
 
+    /// This method is called before the invocation of each test method in the class.
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
