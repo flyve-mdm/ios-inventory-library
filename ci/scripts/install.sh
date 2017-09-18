036f60dcd4b6e6df3f9beb087beb3a8943215e9f
Hector Rondon <hrondon@teclib.com>
2017-09-18 10:22:35 -0400

    ci(circle): install libs dependencies

--- ci/scripts/install.sh
+++ ci/scripts/install.sh
@@ -2,23 +2,23 @@
 
 #   Copyright © 2017 Teclib. All rights reserved.
 #
-# install.sh is part of flyve-mdm-ios-inventory
+# install.sh is part of FlyveMDMInventory
 #
-# flyve-mdm-ios-inventory is a subproject of Flyve MDM. Flyve MDM is a mobile
+# FlyveMDMInventory is a subproject of Flyve MDM. Flyve MDM is a mobile
 # device management software.
 #
-# flyve-mdm-ios-inventory is free software: you can redistribute it and/or
+# FlyveMDMInventory is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 3
 # of the License, or (at your option) any later version.
 #
-# flyve-mdm-ios-inventory is distributed in the hope that it will be useful,
+# FlyveMDMInventory is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # ------------------------------------------------------------------------------
 # @author    Hector Rondon
-# @date      04/09/17
+# @date      25/08/17
 # @copyright Copyright © 2017 Teclib. All rights reserved.
 # @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
 # @link      https://github.com/flyve-mdm/flyve-mdm-ios-inventory
@@ -27,14 +27,14 @@
 
 # Update gem
 gem update --system
-# Install fastlane last version
-gem install fastlane --no-rdoc --no-ri --no-document --quiet
 # Clean Gem
 gem cleanup
 # Install jazzy for generate documentation
 gem install jazzy
-# Install xcov for code coverage reporting
-sudo gem install xcov
+# Install bundler last version
+gem install bundler
+# Install node
+brew install node
 # Install jq for json parse
 brew install jq
 # Install standard-version scope global
@@ -45,3 +45,7 @@ npm install -g conventional-github-releaser
 npm install -g podspec-bump
 # Install libs from package.json
 npm install
+# Install gems from Gemfile
+bundle install --path vendor/bundle
+# Update fastlane plugin
+bundle exec fastlane update_plugins
