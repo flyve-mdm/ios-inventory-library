2136de1e82832d1973f0b37a30f65d8e5e1a4254
Hector Rondon <hrondon@teclib.com>
2017-09-25 15:40:34 -0400

    style(lint): disable rule trailing whitespace

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -32,7 +32,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -63,7 +62,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
