f1e7b80318b94f947999933081a2ff063e85a57e
Hector Rondon <hrondon@teclib.com>
2017-10-17 10:25:40 -0400

    ci(release): update develop branch from current branch

--- ci/scripts/deploy_master.sh
+++ ci/scripts/deploy_master.sh
@@ -110,6 +110,14 @@ if [[ $GH_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version"* &
     # Push commit to origin gh-pages branch
     git push origin gh-pages
 
+    # Update develop branch
+    git branch -D develop
+    git fetch origin develop
+    git checkout develop
+    # Merge master on develop
+    git merge master
+    git push origin develop
+
     # Checkout to release branch
     git checkout $CIRCLE_BRANCH -f
 fi
