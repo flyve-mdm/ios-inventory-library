39a3d7128b7d2c59ac0c8bfb6bad0028a840cfde
Hector Rondon <hrondon@teclib.com>
2017-10-17 13:55:14 -0400

    ci(deploy): validate commit message

--- ci/scripts/deploy_develop.sh
+++ ci/scripts/deploy_develop.sh
@@ -27,78 +27,83 @@
 # @link      https://flyve-mdm.com
 # ------------------------------------------------------------------------------
 
-# Generate CHANGELOG.md and increment version
-npm run release -- -t ''
-# Get version number from package.json
-export GIT_TAG=$(jq -r ".version" package.json)
-# Revert last commit
-git reset --hard HEAD~1
-# Update CFBundleShortVersionString
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${GIT_TAG}" ${PWD}/${APPNAME}/Info.plist
-# Update CFBundleVersion
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $CIRCLE_BUILD_NUM" ${PWD}/${APPNAME}/Info.plist
-# Add modified and delete files
-git add ${APPNAME}/Info.plist
-# Create commit
-git commit -m "ci(beta): generate **beta** for version ${GIT_TAG}"
+GITHUB_COMMIT_MESSAGE=$(git log --format=oneline -n 1 $CIRCLE_SHA1)
 
-# Generate documentation with jazzy
-jazzy
+if [[ $GITHUB_COMMIT_MESSAGE != *"ci(release): generate CHANGELOG.md for version"* && $GITHUB_COMMIT_MESSAGE != *"ci(build): release version"* ]]; then
 
-# Add docs folder
-git add docs -f
-# Create commit, NOTICE: this commit is not sent
-git commit -m "ci(docs): generate **docs** for version ${GIT_TAG}"
+    # Generate CHANGELOG.md and increment version
+    npm run release -- -t ''
+    # Get version number from package.json
+    export GIT_TAG=$(jq -r ".version" package.json)
+    # Revert last commit
+    git reset --hard HEAD~1
+    # Update CFBundleShortVersionString
+    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${GIT_TAG}" ${PWD}/${APPNAME}/Info.plist
+    # Update CFBundleVersion
+    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $CIRCLE_BUILD_NUM" ${PWD}/${APPNAME}/Info.plist
+    # Add modified and delete files
+    git add ${APPNAME}/Info.plist
+    # Create commit
+    git commit -m "ci(beta): generate **beta** for version ${GIT_TAG}"
 
-# Generate code coverage reporting with xcov
-bundle exec fastlane coverage
+    # Generate documentation with jazzy
+    jazzy
 
-# Add coverage folder
-git add coverage -f
-# Create commit, NOTICE: this commit is not sent
-git commit -m "ci(docs): generate **coverage** for version ${GIT_TAG}"
+    # Add docs folder
+    git add docs -f
+    # Create commit, NOTICE: this commit is not sent
+    git commit -m "ci(docs): generate **docs** for version ${GIT_TAG}"
 
-# Update documentation on gh-pages
-git branch -D gh-pages
-git fetch origin gh-pages
-git checkout gh-pages
+    # Generate code coverage reporting with xcov
+    bundle exec fastlane coverage
 
-# Remove old documetation
-rm -rf docs
-rm -rf coverage
+    # Add coverage folder
+    git add coverage -f
+    # Create commit, NOTICE: this commit is not sent
+    git commit -m "ci(docs): generate **coverage** for version ${GIT_TAG}"
 
-git checkout $CIRCLE_BRANCH docs
+    # Update documentation on gh-pages
+    git branch -D gh-pages
+    git fetch origin gh-pages
+    git checkout gh-pages
 
-# Add docs folder
-git add docs
-# Create commit
-git commit -m "ci(docs): generate documentation with jazzy for version ${GIT_TAG}"
+    # Remove old documetation
+    rm -rf docs
+    rm -rf coverage
 
-# Get code coverage from develop branch
-git checkout $CIRCLE_BRANCH coverage
-# Add coverage folder
-git add coverage
-# Create commit
-git commit -m "ci(docs): generate coverage with xcov for version ${GIT_TAG}"
+    git checkout $CIRCLE_BRANCH docs
 
-# Create header content to cache
-echo "---" > header_cache
-echo "cache_version: $CIRCLE_SHA1" >> header_cache
-echo "---" >> header_cache
-# Remove header from file
-sed -e '1,3d' sw.js > cache_file
-rm sw.js
-# Add new header
-(cat header_cache ; cat cache_file) > sw.js
-# Remove temp files
-rm cache_file
-rm header_cache
-# Add sw.js to git
-git add -u
-# Create commit
-git commit -m "ci(cache): force update cache for version ${GIT_TAG}"
+    # Add docs folder
+    git add docs
+    # Create commit
+    git commit -m "ci(docs): generate documentation with jazzy for version ${GIT_TAG}"
 
-# Push commit to origin gh-pages branch
-git push origin gh-pages
+    # Get code coverage from develop branch
+    git checkout $CIRCLE_BRANCH coverage
+    # Add coverage folder
+    git add coverage
+    # Create commit
+    git commit -m "ci(docs): generate coverage with xcov for version ${GIT_TAG}"
 
-git checkout $CIRCLE_BRANCH -f
+    # Create header content to cache
+    echo "---" > header_cache
+    echo "cache_version: $CIRCLE_SHA1" >> header_cache
+    echo "---" >> header_cache
+    # Remove header from file
+    sed -e '1,3d' sw.js > cache_file
+    rm sw.js
+    # Add new header
+    (cat header_cache ; cat cache_file) > sw.js
+    # Remove temp files
+    rm cache_file
+    rm header_cache
+    # Add sw.js to git
+    git add -u
+    # Create commit
+    git commit -m "ci(cache): force update cache for version ${GIT_TAG}"
+
+    # Push commit to origin gh-pages branch
+    git push origin gh-pages
+
+    git checkout $CIRCLE_BRANCH -f
+fi
